bb1e9dc3105199b1518dae07da0cbf5ef9ddfa8f
pedroreis <pedroreiscoder@gmail.com>
2024-02-06 19:43:00 -0300

    Project setup with basic dependencies

---
+++ src/main/java/com/celcoin/credit/BuCreditApplication.java
@@ -0,0 +1,13 @@
+package com.celcoin.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuCreditApplication.class, args);
+	}
+
+}
